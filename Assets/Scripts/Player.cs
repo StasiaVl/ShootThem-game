d9c7e88881bc2642b0912503b531d519724ac8e8
Stasia <v.stasja@gmail.com>
2019-08-10 00:10:03 +0300

    Walking
    Player now can move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody rb;
+    private float xMovement;
+    private float zMovement;
+    private Vector3 direction;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        zMovement = Input.GetAxis("Vertical");
+
+        direction = (xMovement * transform.right + zMovement * transform.forward).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = direction * speed * Time.deltaTime;
+    }
+}
