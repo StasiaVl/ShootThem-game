95e98a1f05448fda999dcaa53f6c699746e26ffc
Stasia <v.stasja@gmail.com>
2019-08-10 20:55:51 +0300

    Follow the mouse
    Player rotates, following the cusor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,18 +10,29 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
     private float xMovement;
     private float zMovement;
+    private float xRotation;
+    private float yRotation;
+    private float minXCamera = -45;
+    private float maxXCamera =  45;
+    private float cameraSens =  5;
     private Vector3 direction;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb  = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        xMovement = Input.GetAxis("Horizontal");
-        zMovement = Input.GetAxis("Vertical");
+        xMovement  = Input.GetAxis("Horizontal");
+        zMovement  = Input.GetAxis("Vertical");
+        xRotation += Input.GetAxis("Mouse Y") * cameraSens;
+        yRotation += Input.GetAxis("Mouse X") * cameraSens;
+
+        xRotation = Mathf.Clamp(xRotation, minXCamera, maxXCamera);
+        transform.localEulerAngles = new Vector3(-xRotation, yRotation, 0);
 
         direction = (xMovement * transform.right + zMovement * transform.forward).normalized;
     }
